018ce95d42c7f3ccbdec2417c247e1a3f0e41c5a
alejandro <alejandro@equipo-compaq.home>
2020-05-24 17:56:51 +0200

    Documentados los getter y setter de la clase cliente.

--- Banco/src/Cliente.java
+++ Banco/src/Cliente.java
@@ -43,59 +43,107 @@ public class Cliente {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+	/**
+	 * Método get para obtener la propiedad "apellido" en cualquier momento
+	 * @return 
+	 */
 	public String getApellido() {
 		return apellido;
 	}
-
+	/**
+	 * Método set con el que establecemos que la variable "apellido" guardará 
+	 * lo mismo que la propiedad con el mismo nombre del objeto
+	 * @param apellido
+	 */
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-
+	/**
+	 * Método get para obtener la propiedad "Apellido2" en cualquier momento
+	 * @return 
+	 */
 	public String getApellido2() {
 		return apellido2;
 	}
-
+	/**
+	 * Método set con el que establecemos que la variable "Apellido2" guardará 
+	 * lo mismo que la propiedad con el mismo nombre del objeto
+	 * @param apellido
+	 */
 	public void setApellido2(String apellido2) {
 		this.apellido2 = apellido2;
 	}
-
+	/**
+	 * Método get para obtener la propiedad "Dirección" en cualquier momento
+	 * @return 
+	 */
 	public String getDireccion() {
 		return direccion;
 	}
-
+	/**
+	 * Método set con el que establecemos que la variable "Direccion" guardará 
+	 * lo mismo que la propiedad con el mismo nombre del objeto
+	 * @param apellido
+	 */
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
-
+	/**
+	 * Método get para obtener la propiedad "NIF" en cualquier momento
+	 * @return 
+	 */
 	public String getNif() {
 		return nif;
 	}
-
+	/**
+	 * Método set con el que establecemos que la variable "NIF" guardará 
+	 * lo mismo que la propiedad con el mismo nombre del objeto
+	 * @param apellido
+	 */
 	public void setNif(String nif) {
 		this.nif = nif;
 	}
-
+	/**
+	 * Método get para obtener la propiedad "Telefono" en cualquier momento
+	 * @return 
+	 */
 	public int getTelefono() {
 		return telefono;
 	}
-
+	/**
+	 * Método set con el que establecemos que la variable "Telefono" guardará 
+	 * lo mismo que la propiedad con el mismo nombre del objeto
+	 * @param apellido
+	 */
 	public void setTelefono(int telefono) {
 		this.telefono = telefono;
 	}
-
+	/**
+	 * Método get para obtener la propiedad "Edad" en cualquier momento
+	 * @return 
+	 */
 	public int getEdad() {
 		return edad;
 	}
-
+	/**
+	 * Método set con el que establecemos que la variable "Edad" guardará 
+	 * lo mismo que la propiedad con el mismo nombre del objeto
+	 * @param apellido
+	 */
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
-
+	/**
+	 * Método get para acceder al array list de cuentas
+	 * @return 
+	 */
 	public ArrayList<Cuenta> getCuentas() {
 		return cuentas;
 	}
-
+	/**
+	 * Método para establecer el array list "cuentas" que usemos como propiedad de clase
+	 * @param cuentas
+	 */
 	public void setCuentas(ArrayList<Cuenta> cuentas) {
 		this.cuentas = cuentas;
 	}
